500144ae9bc6d47541c035d00f3b382033ea40f9
EliVerbrugge <eli.verbrugge@gmail.com>
2018-10-14 15:38:56 -0500

    Styling fixes, added brake function

--- VNH5019.cpp
+++ VNH5019.cpp
@@ -4,9 +4,10 @@ static const int PWM_MIN = 0, PWM_MAX = 255;
 
 VNH5019::VNH5019(const int PwmPin, const int InaPin, const int InbPin, bool upsideDown): INA_PIN(InaPin), INB_PIN(InbPin), PWM_PIN(PwmPin)
 {
-  //brake motor by default
   pinMode(INA_PIN, OUTPUT);
   pinMode(INB_PIN, OUTPUT);
+  
+  //brake motor by default
   digitalWrite(INA_PIN, LOW);
   digitalWrite(INB_PIN, LOW);
   
@@ -16,23 +17,23 @@ VNH5019::VNH5019(const int PwmPin, const int InaPin, const int InbPin, bool upsi
   }
 }
 
-void VNH5019::move(const long movement)
+void VNH5019::drive(uint8_t motor_percent)
 {
-  int mov = movement;
+  int percent = motor_percent;
   int pwm = 0;
     //if mounted upside down then invert the signal passed to it and move accordingly
   if (invert)
   {
-    mov = -mov;
+    percent *=-1;
   }
 
-  currentPower = mov;
+  currentPower = percent;
 
   //if supposed to move backwards
-  if(mov < 0)
+  if(percent < 0)
   {
-    mov = abs(mov);
-    pwm = map(mov, 0, 100, PWM_MIN, PWM_MAX); //map percentage into pwm
+    percent = abs(percent);
+    pwm = map(percent, 0, 100, PWM_MIN, PWM_MAX); //map percentage into pwm
 
     //set InB to 0 and InA to 1 for "reverse" rotation
     digitalWrite(INA_PIN, HIGH);
@@ -42,9 +43,9 @@ void VNH5019::move(const long movement)
   }
 
   //if forwards
-  else if(mov > 0)
+  else if(percent > 0)
   {
-    pwm = map(mov, 0, 100, PWM_MIN, PWM_MAX); //map percentage into pwm
+    pwm = map(percent, 0, 100, PWM_MIN, PWM_MAX); //map percentage into pwm
   
     //set InB to 1 and InA to 0 for forward rotation
     digitalWrite(INA_PIN, LOW);
@@ -54,13 +55,21 @@ void VNH5019::move(const long movement)
   }
 
   //stop
-  else if(mov == 0)
+  else if(percent == 0)
   {
     stop();
   }
-
   
   return;
+}
+
+void VNH5019::brake(uint8_t brake_percent)
+{ 
+  pwm = map(brake_speed, 0, 100, PWM_MIN, PWM_MAX); //map percentage into pwm
+  
+  digitalWrite(INA_PIN, HIGH);
+  digitalWrite(INB_PIN, HIGH); 
+  analogWrite(PWM_PIN, pwm);
 }
 
 long VNH5019::getCurrentMove()
--- VNH5019.h
+++ VNH5019.h
@@ -1,7 +1,8 @@
 #ifndef VNH5019_H_
 #define VNH5019_H_
 
-
+//TODO: Read current values
+//      Adding in structure to support passing in RPM instead of power percent
 class VNH5019
 {
   private:
@@ -12,7 +13,10 @@ class VNH5019
     
     //move function which passes in power percent (which is converted to phase and PWM) to move device
     //could in future be changed to RPM
-    void move(const long movement); 
+    void move(uint8_t motor_percent); 
+    
+    //brake function, in which the percent of motor used to break can specified
+    void VNH5019::brake(uint8_t brake_percent)
         
     //tells device to stop
     void stop();
