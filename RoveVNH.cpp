515502730271ecbd51d87412ed692647ed3e1d24
Malacki Ehlers <ehlers.malacki@gmail.com>
2024-04-27 22:45:32 -0500

    Create RoveVNH

---
+++ RoveVNH.cpp
@@ -0,0 +1,76 @@
+#include "RoveVNH.h"
+
+
+RoveVNH::RoveVNH(const uint8_t pwmPin, const uint8_t forwardPin, const uint8_t reversePin) {
+    m_pwmPin = pwmPin;
+    m_forwardPin = forwardPin;
+    m_reversePin = reversePin;
+
+    m_hasCS = false;
+}
+
+RoveVNH::RoveVNH(const uint8_t pwmPin, const uint8_t forwardPin, const uint8_t reversePin, const uint8_t selectPin, const uint8_t csPin) {
+    m_pwmPin = pwmPin;
+    m_forwardPin = forwardPin;
+    m_reversePin = reversePin;
+    m_selectPin = selectPin;
+    m_csPin = csPin;
+
+    m_hasCS = true;
+}
+
+
+
+#if defined(ARDUINO)
+#include <Arduino.h>
+
+
+void RoveVNH::init() {
+    pinMode(m_forwardPin, OUTPUT);
+    pinMode(m_reversePin, OUTPUT);
+    digitalWrite(m_forwardPin, LOW);
+    digitalWrite(m_reversePin, LOW);
+    analogWrite(m_pwmPin, 0);
+
+    if (m_hasCS) {
+        pinMode(m_selectPin, OUTPUT);
+    }
+}
+
+void RoveVNH::configFrequency(const float frequency) {
+	analogWriteFrequency(m_pwmPin, frequency);
+}
+
+void RoveVNH::configCSScale(float gain) {
+    m_csGain = gain * 3.3 / 1023.0; // convert to ADC scale instead of volts
+}
+
+float RoveVNH::readCurrent() {
+    digitalWrite(m_selectPin, HIGH);
+    delayNanoseconds(50);
+    float current = analogRead(m_csPin) * m_csGain;
+    digitalWrite(m_selectPin, LOW);
+
+    return current;
+}
+
+void RoveVNH::drive(int16_t decipercent) const {
+    decipercent = applyConfigs(decipercent);
+    uint8_t pwm = decipercent * 255 / 1000;
+
+    if (decipercent > 0) {
+        digitalWrite(m_reversePin, LOW);
+        digitalWrite(m_forwardPin, HIGH);
+    } else if (decipercent < 0) {
+        digitalWrite(m_forwardPin, LOW);
+        digitalWrite(m_reversePin, HIGH);
+    } else {
+        digitalWrite(m_forwardPin, LOW);
+        digitalWrite(m_reversePin, LOW);
+    }
+
+	analogWrite(m_forwardPin, pwm);
+}
+
+
+#endif
---
+++ RoveVNH.h
@@ -0,0 +1,76 @@
+#ifndef ROVEVNH_H
+#define ROVEVNH_H
+
+#include "RoveMotor.h"
+
+#include <cstdint>
+
+
+class RoveVNH : public RoveMotor {
+
+private:
+
+	uint8_t m_pwmPin, m_forwardPin, m_reversePin, m_selectPin, m_csPin;
+    bool m_hasCS;
+    float m_csGain = 1;
+
+public:
+
+	/**
+	 * @brief Construct a new RoveHBridge object, without current sensing.
+	 * 
+     * @param pwmPin Arduino pin number for the pwm signal. Must be PWM capable.
+	 * @param forwardPin Arduino pin number for the forward digital output.
+	 * @param reversePin Arduino pin number for the reverse digital output.
+	 */
+	RoveVNH(const uint8_t pwmPin, const uint8_t forwardPin, const uint8_t reversePin);
+
+    /**
+	 * @brief Construct a new RoveHBridge object, with current sensing.
+	 * 
+     * @param pwmPin Arduino pin number for the pwm signal. Must be PWM capable.
+	 * @param forwardPin Arduino pin number for the forward digital output.
+	 * @param reversePin Arduino pin number for the reverse digital output.
+     * @param selectPin Arduino pin number for the current sense select digital output.
+     * @param csPin Arduino pin number for the current sense input. Must be an analog input pin.
+	 */
+	RoveVNH(const uint8_t pwmPin, const uint8_t forwardPin, const uint8_t reversePin, const uint8_t selectPin, const uint8_t csPin);
+
+    /**
+	 * @brief Initialize hardware.
+	 * 
+	 */
+    void init();
+
+	/**
+	 * @brief Configure the frequency used for the PWM signal.
+	 * 
+	 * @param frequency Desired frequency, in Hz.
+	 */
+	void configFrequency(const float frequency);
+
+    /**
+     * @brief Configure the scale of the current sensor.
+     * 
+     * @param gain Conversion factor from CS voltage to motor current (Amps/Volt).
+     */
+    void configCSScale(float gain);
+
+    /**
+     * @brief Read the current draw of the motor.
+     * 
+     */
+    float readCurrent();
+
+	/**
+	 * @brief Write the provided drive signal to the motor.
+	 * 
+	 * @param decipercent Motor output [-1000, 1000].
+	 */
+	void drive(int16_t decipercent) const;
+
+};
+
+
+
+#endif /* ROVEVNH_H */
